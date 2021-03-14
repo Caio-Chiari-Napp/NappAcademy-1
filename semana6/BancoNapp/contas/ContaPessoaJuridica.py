ef323a50156695dc7967746ab53c0dcf76e3329f
Caio Chiari <caio.chiari@napp.ws>
2021-03-14 11:41:22 -0300

    Problema resolvito, todos os testes passaram com sucesso

--- semana6/BancoNapp/contas/ContaPessoaJuridica.py
+++ semana6/BancoNapp/contas/ContaPessoaJuridica.py
@@ -2,4 +2,13 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPessoaJuridica(Conta):
-    pass
+    def __init__(self, **kwargs):
+        super(ContaPessoaJuridica, self).__init__(**kwargs)
+        """
+        Construtor da classe PessoaFísica.
+        Extrai do dicionário kwargs do correntista.
+        """
+        self.empresa = kwargs.get('empresa', '')
+        self.limite = kwargs.get('limite', 1500)
+        
+        
--- semana6/BancoNapp/contas/ContaPoupanca.py
+++ semana6/BancoNapp/contas/ContaPoupanca.py
@@ -2,4 +2,45 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPoupanca(Conta):
-   pass
+    def __init__(self, **kwargs):
+        super(ContaPoupanca, self).__init__(**kwargs)
+        """
+        Construtor da classe PessoaFísica.
+        Extrai do dicionário kwargs do correntista.
+        """
+        self.empresa = kwargs.get('empresa', '')
+        self.profissao = kwargs.get('profissao', '')
+        self.limite = kwargs.get('limite', 0)
+        self.juros = kwargs.get('juros', 0)
+
+        #super(ContaPoupanca, self).__init__(**kwargs)
+      
+    def rendimento_aniversario(self, juros):
+       if juros > 1 or juros < 0:
+         raise ValueError('Os juros precisam ser entre 0 (0%) e 1 (100%).')
+         return
+       self.saldo += (self.saldo * juros)
+
+    def saque(self, valor):
+        """
+        Método para realizar saque.
+        Este método suporta somente números maiores que zero.
+
+        Args:
+            valor (float ou int): Valor positivo do saque
+
+        Raises:
+            ValueError: Erro ocorre quando é informado valor negativo.
+            TypeError: Quando o tipo passado não for inteiro ou float.
+
+        Returns:
+            Float: Valor do saque realizado.
+        """
+        if isinstance(valor, (float, int)):
+            if valor > (self.saldo):
+                raise ValueError('Valor do saque supera seu saldo.')
+                return
+            self.saldo = self.saldo - valor
+            self.extrato.append(('S', valor))
+            return valor
+        raise TypeError('O valor do saque precisa ser numérico')
